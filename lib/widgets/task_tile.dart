d97bfa4f4755d54e734bab42c73f5d4faa496df0
Zahraa <Zahraa@Zahraa>
2023-03-03 22:30:41 +0300

    checkbox state

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,19 +1,52 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     super.key,
   });
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text('this is a task'),
-      trailing: Checkbox(
-        activeColor: Colors.lightBlueAccent,
-        onChanged: (x) {},
-        value: false,
+      title: Text(
+        'this is a task',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckBox(
+        checkBoxState: isChecked,
+        toggleCheckboxState: (checkboxState) {
+          setState(() {
+            isChecked = checkboxState ?? false;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  const TaskCheckBox({
+    super.key,
+    required this.checkBoxState,
+    required this.toggleCheckboxState,
+  });
+  final bool checkBoxState;
+  final Function(bool?)? toggleCheckboxState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      onChanged: toggleCheckboxState,
+      value: checkBoxState,
     );
   }
 }
